60ad6b7d565eee236c6eb84b91263e267cdf0c9b
Primercemovies <primerceug@gmail.com>
2026-04-19 09:53:53 +0300

    Update supabaseClient.ts

--- src/app/context/supabaseClient.ts
+++ src/app/context/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://nlhpnvzpbceolsbozrjw.supabase.co";
-const supabaseAnonKey = "sb_publishable_4k8plBPAQLHH8gF7k8_Zcg__t6-qvz3";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
