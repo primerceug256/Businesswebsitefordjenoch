23d4e019f68a463cff1c184ef87dcd0b3fcc629c
Primercemovies <primerceug@gmail.com>
2026-04-19 14:26:25 +0300

    Update supabaseClient.ts

--- src/app/context/supabaseClient.ts
+++ src/app/context/supabaseClient.ts
@@ -1,4 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import { projectId, publicAnonKey } from "../utils/supabase/info";
+
+// This ensures we point to nlhpnvzpbceolsbozrjw.supabase.co
 
 const supabaseUrl = "https://nlhpnvzpbceolsbozrjw.supabase.co";
 const supabaseAnonKey = "sb_publishable_4k8plBPAQLHH8gF7k8_Zcg__t6-qvz3";
