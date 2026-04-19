c2ef55452a3a6f64df02d248a2628a405e416a71
Primercemovies <primerceug@gmail.com>
2026-04-19 09:25:32 +0300

    Delete supabaseClient.tsx

--- src/supabaseClient.tsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://nlhpnvzpbceolsbozrjw.supabase.co";
-const supabaseAnonKey = "sb_publishable_4k8plBPAQLHH8gF7k8_Zcg__t6-qvz3";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
