b0cc478bacbe8fd80bc8b045689be33346de727b
Primercemovies <primerceug@gmail.com>
2026-04-19 15:43:25 +0300

    Update About.tsx

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -76,7 +76,7 @@ export function About() {
               </div>
               <div className="flex items-center gap-3">
                 <CheckCircle className="w-5 h-5 text-orange-600 flex-shrink-0" />
-                <span className="text-gray-700">All Music Genres & Playlists</span>
+                <span className="text-gray-700">All Music Genres, Movie Genres,Software& Playlists</span>
               </div>
               <div className="flex items-center gap-3">
                 <CheckCircle className="w-5 h-5 text-orange-600 flex-shrink-0" />
