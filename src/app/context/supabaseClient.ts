b9ef15f127d475a208d2208e27369ff3110c6967
Primercemovies <primerceug@gmail.com>
2026-04-19 14:24:44 +0300

    Update supabaseClient.ts

--- src/app/context/supabaseClient.ts
+++ src/app/context/supabaseClient.ts
@@ -3,8 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://nlhpnvzpbceolsbozrjw.supabase.co";
 const supabaseAnonKey = "sb_publishable_4k8plBPAQLHH8gF7k8_Zcg__t6-qvz3";
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase URL or Anon Key");
-}
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
