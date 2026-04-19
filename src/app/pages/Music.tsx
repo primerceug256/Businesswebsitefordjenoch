1f21a690d64f4f0c699cc9fab9f4ba1ce564f88e
Primercemovies <primerceug@gmail.com>
2026-04-19 15:14:53 +0300

    Update Music.tsx

--- src/app/pages/Music.tsx
+++ src/app/pages/Music.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Download, Music as MusicIcon, Play, Pause } from 'lucide-react';
-import { projectId, publicAnonKey } from '/utils/supabase/info';
+import { projectId, publicAnonKey } from "../../../utils/supabase/info";
 
 interface Track {
   id: string;
