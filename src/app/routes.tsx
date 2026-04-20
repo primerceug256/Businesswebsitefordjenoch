31b8d6db580da293026e6bbc76595937fc7d5b81
Primercemovies <primerceug@gmail.com>
2026-04-20 11:44:31 +0300

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import Services from "./pages/Services";
 import Music from "./pages/Music";
 import Movies from "./pages/Movies";
 import Software from "./pages/Software";
@@ -10,12 +9,9 @@ import Subscription from "./pages/Subscription";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Profile from "./pages/Profile";
 import AdminDashboard from "./pages/AdminDashboard";
-import MoviePlayer from "./pages/MoviePlayer";
-import LearnMore from "./pages/LearnMore";
-import NotFound from "./pages/NotFound";
-import MyLibrary from "./pages/MyLibrary"; // Added this import
+import MyLibrary from "./pages/MyLibrary";
+import DJDropOrder from "./pages/DJDropOrder";
 
 export const router = createBrowserRouter([
   {
@@ -23,21 +19,17 @@ export const router = createBrowserRouter([
     Component: Layout,
     children: [
       { index: true, Component: Home },
-      { path: "services", Component: Services },
       { path: "music", Component: Music },
-      { path: "my-library", Component: MyLibrary }, // Path registered here
       { path: "movies", Component: Movies },
-      { path: "movies/watch/:id", Component: MoviePlayer },
       { path: "software", Component: Software },
       { path: "cart", Component: Cart },
       { path: "subscription", Component: Subscription },
       { path: "contact", Component: Contact },
       { path: "login", Component: Login },
       { path: "signup", Component: Signup },
-      { path: "profile", Component: Profile },
       { path: "admin", Component: AdminDashboard },
-      { path: "learn-more", Component: LearnMore },
-      { path: "*", Component: NotFound },
+      { path: "my-library", Component: MyLibrary },
+      { path: "dj-drops", Component: DJDropOrder },
     ],
   },
 ]);
