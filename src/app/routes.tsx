e24b7204e1e7b7fff00e25c7f171f4ef43aa509d
Primercemovies <primerceug@gmail.com>
2026-04-22 02:44:15 +0300

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,47 +1,19 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router"; // CHANGED THIS
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import Services from "./pages/Services";
-import Music from "./pages/Music";
-import Movies from "./pages/Movies";
-import Software from "./pages/Software";
-import Cart from "./pages/Cart";
-import Subscription from "./pages/Subscription";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Profile from "./pages/Profile";
-import AdminDashboard from "./pages/AdminDashboard";
-import MoviePlayer from "./pages/MoviePlayer";
-import LearnMore from "./pages/LearnMore";
-import Nonstop from "./pages/Nonstop";
-import EventBooking from "./pages/EventBooking";
-import DJDrops from "./pages/DJDrops";
-import NotFound from "./pages/NotFound";
+// ... (import your other pages)
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([ // CHANGED THIS
   {
     path: "/",
     Component: Layout,
     children: [
       { index: true, Component: Home },
-      { path: "services", Component: Services },
       { path: "music", Component: Music },
-      { path: "nonstop", Component: Nonstop },
       { path: "movies", Component: Movies },
-      { path: "movies/watch/:id", Component: MoviePlayer },
-      { path: "software", Component: Software },
-      { path: "cart", Component: Cart },
-      { path: "subscription", Component: Subscription },
-      { path: "contact", Component: Contact },
       { path: "login", Component: Login },
       { path: "signup", Component: Signup },
-      { path: "profile", Component: Profile },
-      { path: "admin", Component: AdminDashboard },
-      { path: "learn-more", Component: LearnMore },
-      { path: "event-booking", Component: EventBooking },
-      { path: "dj-drops", Component: DJDrops },
-      { path: "*", Component: NotFound },
+      // ... rest of routes
     ],
   },
 ]);
