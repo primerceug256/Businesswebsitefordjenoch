bcc2491856f49ae005e33388b67911f3dc2b1f57
Primercemovies <primerceug@gmail.com>
2026-04-22 01:45:03 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
     alias: {
       // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
+      '@utils': path.resolve(__dirname, './utils'),
     },
   },
   server: {
