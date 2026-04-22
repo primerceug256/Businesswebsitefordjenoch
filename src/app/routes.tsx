45b9158243a899f8786c11d2ef7d2a1173581cbc
Primercemovies <primerceug@gmail.com>
2026-04-22 09:52:01 +0300

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,13 +1,14 @@
-import { createHashRouter } from "react-router"; // Use HashRouter to prevent 404s
+import { createHashRouter } from "react-router"; // CHANGED FROM createBrowserRouter
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Music from "./pages/Music";
 import Movies from "./pages/Movies";
+import Software from "./pages/Software";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import AdminDashboard from "./pages/AdminDashboard";
 
-export const router = createHashRouter([
+export const router = createHashRouter([ // CHANGED
   {
     path: "/",
     Component: Layout,
@@ -15,6 +16,7 @@ export const router = createHashRouter([
       { index: true, Component: Home },
       { path: "music", Component: Music },
       { path: "movies", Component: Movies },
+      { path: "software", Component: Software },
       { path: "login", Component: Login },
       { path: "signup", Component: Signup },
       { path: "admin", Component: AdminDashboard },
